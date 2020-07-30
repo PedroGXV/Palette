13e046957e0c8740c6d5f83cede0a15a1247c696
Pedirn <63373521+PedroGXV@users.noreply.github.com>
2020-07-30 16:19:03 -0300

    v1.0
    Palette v1.0 uploaded

---
+++ Palette.cs
@@ -0,0 +1,107 @@
+using System;
+
+// using Palette;
+
+namespace Palette {
+    static class Foreground
+    {
+        static public void Black(){
+            Console.ForegroundColor = ConsoleColor.Black;
+        }
+        static public void Blue(){
+            Console.ForegroundColor = ConsoleColor.Blue; 
+        }
+        static public void Cyan(){
+            Console.ForegroundColor = ConsoleColor.Cyan;
+        }
+        static public void DarkBlue(){
+            Console.ForegroundColor = ConsoleColor.DarkBlue; 
+        }
+        static public void DarkCyan(){
+            Console.ForegroundColor = ConsoleColor.DarkCyan; 
+        }
+        static public void DarkGray(){
+            Console.ForegroundColor = ConsoleColor.DarkGray; 
+        }
+        static public void DarkGreen(){
+            Console.ForegroundColor = ConsoleColor.DarkGreen; 
+        }
+        static public void DarkMagenta(){
+            Console.ForegroundColor = ConsoleColor.DarkMagenta; 
+        }
+        static public void DarkRed(){
+            Console.ForegroundColor = ConsoleColor.DarkRed; 
+        }
+        static public void DarkYellow(){
+            Console.ForegroundColor = ConsoleColor.DarkYellow; 
+        }
+        static public void Gray(){
+            Console.ForegroundColor = ConsoleColor.Gray; 
+        }
+        static public void Green(){
+            Console.ForegroundColor = ConsoleColor.Green;
+        }
+        static public void Magenta(){
+            Console.ForegroundColor = ConsoleColor.Magenta; 
+        }
+        static public void Red(){
+            Console.ForegroundColor = ConsoleColor.Red; 
+        }
+        static public void White(){
+            Console.ForegroundColor = ConsoleColor.White; 
+        }
+        static public void Yellow(){
+            Console.ForegroundColor = ConsoleColor.Yellow; 
+        }
+    }
+    static class Background {
+        static public void Black(){
+            Console.BackgroundColor = ConsoleColor.Black; 
+        }
+        static public void Blue(){
+            Console.BackgroundColor = ConsoleColor.Blue; 
+        }
+        static public void Cyan(){
+            Console.BackgroundColor = ConsoleColor.Cyan; 
+        }
+        static public void DarkBlue(){
+            Console.BackgroundColor = ConsoleColor.DarkBlue; 
+        }
+        static public void DarkCyan(){
+            Console.BackgroundColor = ConsoleColor.DarkCyan; 
+        }
+        static public void DarkGray(){
+            Console.BackgroundColor = ConsoleColor.DarkGray; 
+        }
+        static public void DarkGreen(){
+            Console.BackgroundColor = ConsoleColor.DarkGreen; 
+        }
+        static public void DarkMagenta(){
+            Console.BackgroundColor = ConsoleColor.DarkMagenta; 
+        }
+        static public void DarkRed(){
+            Console.BackgroundColor = ConsoleColor.DarkRed; 
+        }
+        static public void DarkYellow(){
+            Console.BackgroundColor = ConsoleColor.DarkYellow; 
+        }
+        static public void Gray(){
+            Console.BackgroundColor = ConsoleColor.Gray; 
+        }
+        static public void Green(){
+            Console.BackgroundColor = ConsoleColor.Green; 
+        }
+        static public void Magenta(){
+            Console.BackgroundColor = ConsoleColor.Magenta; 
+        }
+        static public void Red(){
+            Console.BackgroundColor = ConsoleColor.Red; 
+        }
+        static public void White(){
+            Console.BackgroundColor = ConsoleColor.White; 
+        }
+        static public void Yellow(){
+            Console.BackgroundColor = ConsoleColor.Yellow; 
+        }
+    }
+}
---
+++ read_me.txt
@@ -0,0 +1,3 @@
+// ReadMe:
+
+// This file (Palette.cs) was created to use with .Net Framework 4, but you can use as you want
